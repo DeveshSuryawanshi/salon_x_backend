12bd7d9a4f0f0e7ebba4aa4205f47344348f0891
Devesh Suryawanshi <110163013+DeveshSuryawanshi@users.noreply.github.com>
2025-01-03 16:15:49 +0530

    initial commit - 1

---
+++ src/config/config.mjs
@@ -0,0 +1,45 @@
+import dotenv from "dotenv";
+import ENV from "./env.mjs";
+dotenv.config();
+
+const config = {
+    app: {
+        port: ENV.PORT,
+        nodeEnv: ENV.NODE_ENV,
+    },
+    mongoDB: {
+        uri: ENV.MONGODB_URI,
+        user: ENV.MONGODB_USER,
+        password: ENV.MONGODB_PASSWORD,
+        host: ENV.MONGODB_HOST,
+        port: ENV.MONGODB_PORT,
+        name: ENV.MONGODB_NAME,
+    },
+    mysql: {
+        host: ENV.DB_HOST,
+        port: ENV.DB_PORT,
+        name: ENV.DB_NAME,
+        user: ENV.DB_USER,
+        password: ENV.DB_PASSWORD,
+    },
+    jwt: {
+        secret: ENV.JWT_SECRET,
+    },
+    session: {
+        secret: ENV.SESSION_SECRET,
+    },
+    log: {
+        level: ENV.LOG_LEVEL,
+        file: ENV.LOG_FILE,
+    },
+    cors: {
+        origin: ENV.CORS_ORIGIN,
+    },
+    smtp: {
+        host: ENV.SMTP_HOST,
+        user: ENV.SMTP_USER,
+        password: ENV.SMTP_PASSWORD,
+    },
+};
+
+export default config;
---
+++ src/config/env.mjs
@@ -0,0 +1,30 @@
+const ENV = {
+    NODE_ENV: process.env.NODE_ENV || "development",
+    PORT: process.env.PORT || 3000, // Fallback to 3000 if not set
+    // MongoDB URI (recommended approach)
+    MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost:27017/myapp",
+    // if you need to use authentication (optional)
+    MONGODB_USER: process.env.MONGODB_USER || "admin",
+    MONGODB_PASSWORD: process.env.MONGODB_PASSWORD || "password",
+    MONGODB_HOST: process.env.MONGODB_HOST || "localhost",
+    MONGODB_PORT: process.env.MONGODB_PORT || 27017,
+    MONGODB_NAME: process.env.MONGODB_NAME || "myDatabase",
+    // if you want to use MySQL
+    DB_HOST: process.env.DB_HOST || "localhost",
+    DB_PORT: process.env.DB_PORT || 3306,
+    DB_NAME: process.env.DB_NAME || "myapp",
+    DB_USER: process.env.DB_USER || "root",
+    DB_PASSWORD: process.env.DB_PASSWORD || "password",
+    // JWT secret key (required)
+    JWT_SECRET: process.env.JWT_SECRET || "default_jwt_secret",
+    // Other environment variables
+    SESSION_SECRET: process.env.SESSION_SECRET,
+    LOG_LEVEL: process.env.LOG_LEVEL || "info",
+    LOG_FILE: process.env.LOG_FILE || "app.log",
+    CORS_ORIGIN: process.env.CORS_ORIGIN || "http://localhost:3000",
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_USER: process.env.SMTP_USER,
+    SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+};
+
+export default ENV;
